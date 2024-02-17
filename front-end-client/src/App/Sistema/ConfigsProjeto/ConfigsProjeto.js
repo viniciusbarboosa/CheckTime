5906fb436c9d7c8e8974524268bfc0f47a0547d0
viniciusbarboosa <vindzn88@gmail.com>
2024-02-16 21:17:34 -0300

    Stat Mui e criacao de rotas

---
+++ front-end-client/src/App/Sistema/ConfigsProjeto/ConfigsProjeto.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConfigsProjeto = () => {
+  return (
+    <div>ConfigsProjeto</div>
+  )
+}
+
+export default ConfigsProjeto
--- front-end-client/src/index.js
+++ front-end-client/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from './App/Sistema/Dashboard';
+import ConfigsProjeto from './App/Sistema/ConfigsProjeto/ConfigsProjeto';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard></Dashboard>,
+    children: [
+      {
+        path: "/ConfigsProjeto",
+        element: <ConfigsProjeto></ConfigsProjeto>,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
